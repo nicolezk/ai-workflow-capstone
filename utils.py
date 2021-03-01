a3b2d1fea72fdd32c649762cc9dd688990dda279
Nicole Zafalon Kovacs <nicole@ibm.com>
2021-03-01 20:09:55 -0300

    add read function

---
+++ utils.py
@@ -0,0 +1,61 @@
+import os
+import re
+import pandas as pd
+
+def load_json_from_dir(dir):
+    """
+    Reads multiple json files in a directory, aggregates them, and sorts them by date.
+
+    Parameters
+    ----------
+    dir: directory where the json files are
+
+    Returns
+    -------
+    df: pandas DataFrame with datetimeindex
+    """
+
+    ## argument check
+    if not os.path.isdir(dir):
+        raise Exception('Input specified is not a valid directory')
+    elif not len(os.listdir(dir)) > 0:
+        raise Exception('Specified directory does not contain files')
+
+    file_list = [os.path.join(dir, f) for f in os.listdir(dir) if re.search('\.json', f)]
+
+    if not len(file_list) > 0:
+        raise Exception('Specified directory does not contain json files')
+
+    ## format columns and read files
+    sorted_columns = ['country', 'customer_id', 'day', 'invoice', 'month',
+                      'price', 'stream_id', 'times_viewed', 'year']
+    
+    df = None
+
+    for f in file_list:
+        _df = pd.read_json(f)
+
+        ## common column typos
+        _df = _df.rename(columns={'StreamID':'stream_id',
+                                  'TimesViewed':'times_viewed',
+                                  'total_price':'price'})
+
+        if sorted(list(_df.columns)) != sorted_columns:
+            raise Exception(f'Columns of file {f} could not be matched to the template \
+                              (file columns: {list(_df.columns)})')
+
+        _df['date'] = pd.to_datetime(_df[['year','month','day']])
+        _df = _df.drop(columns=['year','month','day'])
+        _df['invoice'] = _df['invoice'].str.replace(r'\D', '') # removes non digit characters from invoice id
+
+        df = pd.concat([df,_df])
+
+    df = df.set_index(pd.DatetimeIndex(df['date']))
+    df = df.drop(columns=['date'])
+    df = df.sort_index()
+
+    return df
+
+
+
+load_json_from_dir('/Users/nicole.zafalon.kovacs/git/ai-workflow-capstone/0_exercise_files/cs-production')
